e6536451ba36d3b0c6c7e5baefaa52210432abca
vedikabinani10 <69752144+vedikabinani10@users.noreply.github.com>
2020-10-27 18:20:51 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,17 @@
+class Box {
+    constructor(x, y, width, height) {
+      var options ={
+        restitution:0.04,
+        friction:1.0,
+        density:0.004
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+       rectMode(CENTER);
+       rect(this.body.position.x,this.body.position.y,this.width,this.height);
+  }
+}
